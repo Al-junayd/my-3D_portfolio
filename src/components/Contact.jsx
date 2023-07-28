1c3075a870a7a5dadec63778227bf62407c65a27
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2023-07-28 01:14:38 +0100

    finalized

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+            className="bg-tertiary py-4 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
             Send{" "}
           </button>
